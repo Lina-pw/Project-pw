cc7c560df5eb82058d0202d2b487add4f5908d1d
Lina-pw <lina.ccora@playwithlatam.com>
2025-06-27 11:38:18 -0500

    Update app_retention_upexcel.py

--- app_retention_upexcel.py
+++ app_retention_upexcel.py
@@ -12,7 +12,7 @@ st.title("Calcule Retention UP Excel")
 uploaded_file = st.file_uploader("Upload your Excel file (.xlsx)", type=["xlsx"])
 
 # Nombre del archivo de salida
-output_filename = st.text_input("Nombre del archivo de salida (sin .xlsx)", value="retention_result")
+output_filename = st.text_input("Output file name (without .xlsx)", value="retention_result")
 
 if uploaded_file and output_filename:
     # Leer Excel
